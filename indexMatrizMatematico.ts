f66dc0b6d2d70ffca59eadb2f9dd54fc9bf16349
lucasgfaj <lucasgfajardo09@gmail.com>
2024-06-18 00:13:59 -0300

    ordenaIterativa

--- indexMatrizMatematico.ts
+++ indexMatrizMatematico.ts
@@ -7,20 +7,28 @@ console.log("Matriz Original - toString");
 console.log(matriz.toString());
 console.log();
 
-console.log("VerificaInteiro")
-console.log(matriz.verificaInteiro(6));
+// console.log("VerificaInteiro")
+// console.log(matriz.verificaInteiro(6));
+// console.log();
+
+// console.log("verificaOcorrência")
+// console.log(matriz.verificaOcorrencia(5));
+// console.log();
+
+// console.log("somaInteiro")
+// console.log(matriz.somaInteiros());
+// console.log();
+
+// console.log("listEvenOrOdd")
+// console.log(matriz.listEvenOrOdd());
+// console.log();
+
+console.log("OrdenaIterativa")
+console.log(matriz.ordenaIterativa());
 console.log();
 
-console.log("verificaOcorrência")
-console.log(matriz.verificaOcorrencia(5));
-console.log();
-
-console.log("somaInteiro")
-console.log(matriz.somaInteiros());
-console.log();
-
-console.log("listEvenOrOdd")
-console.log(matriz.listEvenOrOdd());
+console.log("Matriz Original - toString");
+console.log(matriz.toString());
 console.log();
 
 //Matriz Jardim de Infância
@@ -29,102 +37,102 @@ const turma = new matrizJardim();
 // "Luan", "Antonella", "José", "Lucas", "Pedro", 
 // "Antonio", "João", "Eleandro", "Josué"
 
-console.log("AdicionarAluno")
-console.log(turma.adicionarAluno("Moisés"));
-console.log(turma.adicionarAluno("Lucas"));
-console.log(turma.adicionarAluno("Eleandro"));
-console.log(turma.adicionarAluno("João"));
-console.log(turma.adicionarAluno("Laira"));
-console.log(turma.adicionarAluno("Lucas"));
-console.log(turma.adicionarAluno("Guilherme"));
-console.log(turma.adicionarAluno("Antônio"));
-console.log(turma.adicionarAluno("Isabeli"));
-console.log(turma.adicionarAluno("Pedro"));
+// console.log("AdicionarAluno")
+// console.log(turma.adicionarAluno("Moisés"));
+// console.log(turma.adicionarAluno("Lucas"));
+// console.log(turma.adicionarAluno("Eleandro"));
+// console.log(turma.adicionarAluno("João"));
+// console.log(turma.adicionarAluno("Laira"));
+// console.log(turma.adicionarAluno("Lucas"));
+// console.log(turma.adicionarAluno("Guilherme"));
+// console.log(turma.adicionarAluno("Antônio"));
+// console.log(turma.adicionarAluno("Isabeli"));
+// console.log(turma.adicionarAluno("Pedro"));
 
-console.log();
+// console.log();
 
-console.log("toString");
-console.log(turma.toString());
-console.log();
+// console.log("toString");
+// console.log(turma.toString());
+// console.log();
 
-console.log("numeroAluno");
-console.log(turma.numeroAluno(5));
-console.log();
+// console.log("numeroAluno");
+// console.log(turma.numeroAluno(5));
+// console.log();
 
 
-console.log("nomeAluno");
-console.log(turma.nomeAluno("Luan"));
-console.log();
+// console.log("nomeAluno");
+// console.log(turma.nomeAluno("Luan"));
+// console.log();
 
-console.log("nomeAluno");
-console.log(turma.listAlunos());
-console.log();
+// console.log("nomeAluno");
+// console.log(turma.listAlunos());
+// console.log();
 
 // Criando a instância da classe matrizCarros
 const matrizCar = new matrizCarros();
 
-console.log();
-console.log("Adicionar Carro");
-matrizCar.adicionarCarro("Ford Ka", 20);
-matrizCar.adicionarCarro("Fusca", 70);
-matrizCar.adicionarCarro("Chevette", 200);
-matrizCar.adicionarCarro("Opala", 190);
-matrizCar.adicionarCarro("Astra", 150);
-matrizCar.adicionarCarro("Gol", 100);
-matrizCar.adicionarCarro("Caravan", 130);
-matrizCar.adicionarCarro("HB20", 110);
-matrizCar.adicionarCarro("Versa", 139);
-matrizCar.adicionarCarro("Corolla", 120);
-console.log();
+// console.log();
+// console.log("Adicionar Carro");
+// matrizCar.adicionarCarro("Ford Ka", 20);
+// matrizCar.adicionarCarro("Fusca", 70);
+// matrizCar.adicionarCarro("Chevette", 200);
+// matrizCar.adicionarCarro("Opala", 190);
+// matrizCar.adicionarCarro("Astra", 150);
+// matrizCar.adicionarCarro("Gol", 100);
+// matrizCar.adicionarCarro("Caravan", 130);
+// matrizCar.adicionarCarro("HB20", 110);
+// matrizCar.adicionarCarro("Versa", 139);
+// matrizCar.adicionarCarro("Corolla", 120);
+// console.log();
 
-// Exibindo a matriz de carros
-console.log(matrizCar.toString());
-console.log();
+// // Exibindo a matriz de carros
+// console.log(matrizCar.toString());
+// console.log();
 
-//MetodoMedia
-console.log("Soma de Velocidades")
-console.log(matrizCar.mediaAtingida());
-console.log();
+// //MetodoMedia
+// console.log("Soma de Velocidades")
+// console.log(matrizCar.mediaAtingida());
+// console.log();
 
-//SuperiorMedia
-console.log("Superior Média Atingida")
-console.log(matrizCar.superiorMediaAtingida());
-console.log();
+// //SuperiorMedia
+// console.log("Superior Média Atingida")
+// console.log(matrizCar.superiorMediaAtingida());
+// console.log();
 
-//Maior Velocidade
-console.log("Maior Velocidade")
-console.log(matrizCar.maiorVelocidadeTeste());
-console.log();
+// //Maior Velocidade
+// console.log("Maior Velocidade")
+// console.log(matrizCar.maiorVelocidadeTeste());
+// console.log();
 
-// Testando o Autódromo
-const autodromoTeste = new Autodromo();
+// // Testando o Autódromo
+// const autodromoTeste = new Autodromo();
 
-autodromoTeste.adicionaCarroAutodromo(new CarroCorrida("Porsche 911", 311.5));
-autodromoTeste.adicionaCarroAutodromo(new CarroCorrida("Ferrari Enzo", 350));
-autodromoTeste.adicionaCarroAutodromo(new CarroCorrida("Lamborghini Aventador", 355));
-autodromoTeste.adicionaCarroAutodromo(new CarroCorrida("Bugatti Veyron", 407));
-autodromoTeste.adicionaCarroAutodromo(new CarroCorrida("McLaren P1", 350));
-autodromoTeste.adicionaCarroAutodromo(new CarroCorrida("Koenigsegg Agera", 447));
-autodromoTeste.adicionaCarroAutodromo(new CarroCorrida("Pagani Huayra", 383));
-autodromoTeste.adicionaCarroAutodromo(new CarroCorrida("Ford GT", 348));
-autodromoTeste.adicionaCarroAutodromo(new CarroCorrida("Chevrolet Corvette", 330));
-autodromoTeste.adicionaCarroAutodromo(new CarroCorrida("Nissan GT-R", 315));
+// autodromoTeste.adicionaCarroAutodromo(new CarroCorrida("Porsche 911", 311.5));
+// autodromoTeste.adicionaCarroAutodromo(new CarroCorrida("Ferrari Enzo", 350));
+// autodromoTeste.adicionaCarroAutodromo(new CarroCorrida("Lamborghini Aventador", 355));
+// autodromoTeste.adicionaCarroAutodromo(new CarroCorrida("Bugatti Veyron", 407));
+// autodromoTeste.adicionaCarroAutodromo(new CarroCorrida("McLaren P1", 350));
+// autodromoTeste.adicionaCarroAutodromo(new CarroCorrida("Koenigsegg Agera", 447));
+// autodromoTeste.adicionaCarroAutodromo(new CarroCorrida("Pagani Huayra", 383));
+// autodromoTeste.adicionaCarroAutodromo(new CarroCorrida("Ford GT", 348));
+// autodromoTeste.adicionaCarroAutodromo(new CarroCorrida("Chevrolet Corvette", 330));
+// autodromoTeste.adicionaCarroAutodromo(new CarroCorrida("Nissan GT-R", 315));
 
-console.log("Carros no Autódromo:");
-console.log(autodromoTeste.getListagem());
-console.log();
+// console.log("Carros no Autódromo:");
+// console.log(autodromoTeste.getListagem());
+// console.log();
 
-console.log("Media Atingida no Autódromo:");
-console.log(autodromoTeste.mediaAtingidaAutodromo());
-console.log();
+// console.log("Media Atingida no Autódromo:");
+// console.log(autodromoTeste.mediaAtingidaAutodromo());
+// console.log();
 
-console.log("Carros Superior a Media Atingida no Autódromo:");
-console.log(autodromoTeste.superiorMediaAutodromo());
-console.log();
+// console.log("Carros Superior a Media Atingida no Autódromo:");
+// console.log(autodromoTeste.superiorMediaAutodromo());
+// console.log();
 
-console.log("Carro com a mairo velocidade no Autódromo:");
-console.log(autodromoTeste.maiorVelocidadeAutodromo());
-console.log();
+// console.log("Carro com a mairo velocidade no Autódromo:");
+// console.log(autodromoTeste.maiorVelocidadeAutodromo());
+// console.log();
 
 //Matriz Aleatoria
 const aleatoria = new MatrizAleatoria(3, 3);
--- matrizMatematico.ts
+++ matrizMatematico.ts
@@ -71,6 +71,20 @@ export class matrizUni {
         }
         return result.join("\n");
     }
+
+    public ordenaIterativa(): void{
+
+        for(let i = 0; i < this.arr.length - 1; i++){
+            for(let j = 0; j < this.arr.length - 1 - i; j++){
+                if(this.arr[j] > this.arr[j + 1]){
+                    let temp = this.arr[j];
+                    this.arr[j] = this.arr[j + 1];
+                    this.arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
 }
 
 // Matriz Jardim de Infância
